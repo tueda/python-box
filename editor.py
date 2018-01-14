9d971126735a0b8feecb7eeed98ac31ffe953176
Takahiro Ueda <tueda@nikhef.nl>
2018-01-14 21:01:27 +0100

    Automatically load/save the text in the editor

--- editor.py
+++ editor.py
@@ -6,14 +6,37 @@ import traceback
 from browser import document, window
 
 
+jq = window.jQuery
 editor = window.ace.edit('editor')
 console = document['console']
 run_button = document['runButton']
 
+# Editor setting.
 
 editor.getSession().setMode('ace/mode/python')
 editor.focus()
 
+# HTML 5 local storage
+
+if hasattr(window, 'localStorage'):
+    from browser.local_storage import storage
+else:
+    storage = None
+
+if storage is not None and 'py_src' in storage:
+    editor.setValue(storage['py_src'], -1)
+
+
+def on_window_unload(_e):
+    """Event handler for exiting the page."""
+    if storage is not None:
+        storage['py_src'] = editor.getValue()
+
+
+jq(window).on('unload', on_window_unload)
+
+
+# Console output.
 
 class ConsoleOutput:
     """Console output."""
@@ -30,6 +53,8 @@ class ConsoleOutput:
 sys.stdout = ConsoleOutput()
 sys.stderr = ConsoleOutput()
 
+
+# Running the script.
 
 @run_button.bind('click')
 def run_python(_e):
